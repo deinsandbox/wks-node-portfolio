475ec7526a13f1c6ff0fbd7d7d728d24b85fb2aa
equiman <equiman@users.noreply.github.com>
2020-07-18 23:36:08 -0500

    Add simple icons get/post

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const simpleIcons = require("simple-icons");
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,34 @@ const portfolio = require("./data/portfolio.json");
 
 app.get("/", (req, res) => {
   res.send("It's Alive");
+});
+
+app.get("/icon", (req, res) => {
+  try {
+    const { name } = req.query;
+    const icon = simpleIcons.get(name);
+    if (!icon) {
+      throw new Error(`"${name}" icon not exists.`);
+    }
+
+    res.status(200).json(icon);
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+});
+
+app.post("/icon", (req, res) => {
+  try {
+    const { name } = req.body;
+    const icon = simpleIcons.get(name);
+    if (!icon) {
+      throw new Error(`"${name}" icon not exists.`);
+    }
+
+    res.status(200).json(icon);
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
 });
 
 app.get("/about", (req, res) => {
